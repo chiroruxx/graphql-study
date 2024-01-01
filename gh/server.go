382c4c088f4b682b6e123f387102c88782127fb4
chihiro <dormir.kazu@gmail.com>
2024-01-01 23:17:01 +0900

    ch4

--- gh/server.go
+++ gh/server.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/chiroruxxxx/graphql-study/gh/graph"
+	"github.com/chiroruxxxx/graphql-study/gh/graph/services"
 	"github.com/chiroruxxxx/graphql-study/gh/internal"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-const defaultPort = "8080"
+const (
+	defaultPort = "8080"
+	dbFile      = "./mygraphql.db"
+)
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,7 +28,25 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{}}))
+	db, err := sql.Open("sqlite3", fmt.Sprintf("%s?_foreign_keys=on", dbFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	service := services.New(db)
+
+	directiveRoot := internal.DirectiveRoot{
+		IsAuthenticated: func(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
+			return next(ctx)
+		},
+	}
+
+	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{
+		Directives: directiveRoot,
+		Resolvers: &graph.Resolver{
+			Srv: service,
+		}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
