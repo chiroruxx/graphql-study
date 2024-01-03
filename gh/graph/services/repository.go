3c9ccf5e1a588169e60feca28cec830afff153f4
chihiro <dormir.kazu@gmail.com>
2024-01-04 00:47:42 +0900

    Change column type from TIMESTAMP to DATETIME

--- gh/graph/services/repository.go
+++ gh/graph/services/repository.go
@@ -14,7 +14,7 @@ func convertRepository(repository *db.Repository) *model.Repository {
 		ID: repository.ID,
 		//Owner:        repository.Owner,
 		Name:      repository.Name,
-		CreatedAt: repository.CreatedAt,
+		CreatedAt: repository.CreatedAt.String(),
 		//Issue:        repository.Issues(),
 		//Issues:       repository.Issues(),
 		//PullRequest:  repository.Pullrequests(),
